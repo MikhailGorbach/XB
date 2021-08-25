75898843cc0751e249e357dbc1133782013ef204
MikhailGorbach <michagorbach57@gmail.com>
2021-08-25 12:06:14 +0300

    123

---
+++ FirebasePicture/app/src/main/java/com/example/firebasepicture/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.firebasepicture;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.assets.RenderableSource;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.*;
+import com.google.firebase.storage.StorageTask;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.graphics.Bitmap;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
+    private StorageReference mStorageReference; // НЕ ТРОГАТЬ!!!!!!!!!!
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // !!!!!!!! КНОПКА ФРАГМЕНТА !!!!!!!!!!!!
+
+        FragmentForButton fragmentForButton = new FragmentForButton();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, fragmentForButton).commit();
+
+        //!!!!!!!!!!!!!!   НЕ ТРОГРАТЬ         !!!!!!!!!!!! МАГИЯ   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! НАЧАЛО
+
+        //Подключаем Firebase для картинки
+        mStorageReference = FirebaseStorage.getInstance().getReference().child("1597154303_00018.jpg");
+        try {
+            final File localFile = File.createTempFile("1597154303_00018", "jpg");
+            mStorageReference.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(MainActivity.this, "Picture Retrieved", Toast.LENGTH_SHORT).show();
+                            Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            ((ImageView)findViewById(R.id.imageView)).setImageBitmap(bitmap);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, "Error Occurred", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        //Подключаем Firebase для модельки
+
+        FirebaseApp.initializeApp(this);
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference modelRef = storage.getReference().child("blasterH.glb");
+
+        ArFragment arFragment = (ArFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.arFragment);
+
+        findViewById(R.id.downloadBtn)
+                .setOnClickListener(v -> {
+
+                    try {
+                        File file = File.createTempFile("blasterH", "glb");
+
+                        modelRef.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+
+                                buildModel(file);
+
+                            }
+                        });
+
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+
+                });
+
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+
+            AnchorNode anchorNode = new AnchorNode(hitResult.createAnchor());
+            anchorNode.setRenderable(renderable);
+            arFragment.getArSceneView().getScene().addChild(anchorNode);
+
+        });
+    }
+    private ModelRenderable renderable;
+
+    private void buildModel(File file) {
+        try {
+            RenderableSource renderableSource = RenderableSource
+                    .builder()
+                    .setSource(this, Uri.parse(file.getPath()), RenderableSource.SourceType.GLB)
+                    .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                    .build();
+
+            ModelRenderable
+                    .builder()
+                    .setSource(this, renderableSource)
+                    .setRegistryId(file.getPath())
+                    .build()
+                    .thenAccept(modelRenderable -> {
+                        Toast.makeText(this, "Model built", Toast.LENGTH_SHORT).show();
+                        renderable = modelRenderable;
+                    });
+        }
+        catch (@NonNull Exception e)
+        {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "Error Occurred", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!  ТУТ МАГИЯ ЗАКОЧИЛАСЬ  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+}
