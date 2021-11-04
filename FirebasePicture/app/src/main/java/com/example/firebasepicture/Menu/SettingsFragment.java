4a8d097e8a9632b8eed743545b9a77f6ef34b671
ipon1105 <71529066+ipon1105@users.noreply.github.com>
2021-11-04 10:43:20 +0300

    Edit the SettingFragment
    Add into fragment_settings.xml ImageButtom with ShareIt

--- FirebasePicture/app/src/main/java/com/example/firebasepicture/Menu/SettingsFragment.java
+++ FirebasePicture/app/src/main/java/com/example/firebasepicture/Menu/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.firebasepicture.Menu;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,7 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.firebasepicture.Policy.PrivatePolicyFragment;
 import com.example.firebasepicture.R;
@@ -18,6 +21,7 @@ public class SettingsFragment extends Fragment {
     private TextView txtLinkSettingFeedBack;
     private TextView txtLinkSettingScenes;
     private SettingsFragment ctx;
+    private ImageButton btnImgShareIt;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,6 +36,7 @@ public class SettingsFragment extends Fragment {
     private void initComponent(View v){
         ctx = this;
 
+        btnImgShareIt = v.findViewById(R.id.imgBtnShareIt);
         txtLinkSettingPrivatePolicy = v.findViewById(R.id.txtLinkSettingPrivatePolicy);
         txtLinkSettingTermsAndConditions = v.findViewById(R.id.txtLinkSettingTermsAndConditions);
         txtLinkSettingFeedBack = v.findViewById(R.id.txtLinkSettingFeedBack);
@@ -57,6 +62,24 @@ public class SettingsFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 getFragmentManager().beginTransaction().replace(R.id.fragment_container, new PrivatePolicyFragment(ctx,2)).commit();
+            }
+        });
+
+        btnImgShareIt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                String textToSend="some text";
+                intent.putExtra(Intent.EXTRA_TEXT, textToSend);
+                try
+                {
+                    startActivity(Intent.createChooser(intent, "Описание действия"));
+                }
+                catch (android.content.ActivityNotFoundException ex)
+                {
+                    Toast.makeText(getContext(), "Some error", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
